2db8435f8795f171d5f31e1fb1a70570535f7a97
Kelvin Pereira Biserra <42652204+Kelvin-Pereira@users.noreply.github.com>
2022-09-08 15:18:16 -0300

    Inicio Teste Unitário Produto

---
+++ src/test/java/com/mercado/mercadinho/service/ProdutoServiceTest.java
@@ -0,0 +1,46 @@
+package com.mercado.mercadinho.service;
+
+import com.mercado.mercadinho.domain.entity.Produto;
+import com.mercado.mercadinho.repository.ProdutoRepository;
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+
+@ActiveProfiles("local")
+@SpringBootTest
+@RunWith(SpringRunner.class)
+class ProdutoServiceTest {
+
+    @Mock
+    private ProdutoRepository repository;
+
+    @InjectMocks
+    private ProdutoService service;
+
+    @Test
+    void testBuscarPorId() {
+        criarRetornoProdutoByIdRepository();
+        service.findById(1L);
+    }
+
+    private void criarRetornoProdutoByIdRepository(){
+        Produto produto = new Produto();
+        produto.setId(1L);
+        produto.setNome("Carne");
+        produto.setValor(10.23);
+        produto.setDescricao("Carne de Carneiro");
+        produto.setIsAtivo(Boolean.TRUE);
+        Optional<Produto> optionalProduto = Optional.of(produto);
+        Mockito.doReturn(optionalProduto).when(repository).findById(any(Long.class));
+    }
+}
